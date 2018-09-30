060c681345ba80a65e75ea3864cc5118f67bc566
Clyde Mendonca <clyde.mendonca01@gmail.com>
2018-09-30 11:24:58 +0530

    import LinearRegression addded

--- Clyde_linear_regression_walkthrough.py
+++ Clyde_linear_regression_walkthrough.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
 
 
 # In[2]:
